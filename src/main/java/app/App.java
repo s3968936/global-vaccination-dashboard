13ae29fa1d71b37bac0b4eeede497faf171ad089
Kheang027 <150226091+Kheang027@users.noreply.github.com>
2025-10-24 11:40:39 +1100

    move top-nav to the rigth

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -47,14 +47,12 @@ public class App {
         
         // ADD ALL OF YOUR WEBPAGES HERE
         app.get(PageIndex.URL, new PageIndex(connection));
-        app.get(PageMoviesList.URL, new PageMoviesList(connection));      
-        app.get(PageMoviesType.URL, new PageMoviesType(connection));
         app.get(ExploreDataPage.URL, new ExploreDataPage(connection));
         app.get(TrendingPage.URL, new TrendingPage(connection));
         app.get(InsightsPage.URL, new InsightsPage(connection));
 
         // POST pages can accept form data
-        app.post(PageMoviesType.URL, new PageMoviesType(connection));
+        
     }
 
 }
